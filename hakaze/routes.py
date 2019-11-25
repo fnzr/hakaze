e884772ce9e9694d9d53e42bcf8e4c2668c4b4bc
SANGVIS <5471818+fnzr@users.noreply.github.com>
2019-11-25 17:36:44 -0300

    Request pages paths

--- hakaze/routes.py
+++ hakaze/routes.py
@@ -24,7 +24,30 @@ def covers():
     return jsonify(list(mongo.db.galleries.aggregate(pipeline)))
 
 
-@root.route("/countGalleries", methods=["POST"])
+@root.route("/pages", methods=["POST"])
+def pages():
+    data = request.get_json(force=True)
+    limit = data["limit"] if "limit" in data else 20
+    skip = data["offset"] if "offset" in data else 0
+
+    pipeline = [
+        {"$match": {"_id": data["dir"]}},
+        {
+            "$project": {
+                "pages": {
+                    "$map": {
+                        "input": {"$slice": ["$pages_ar", skip, limit]},
+                        "as": "page",
+                        "in": {"$concat": ["$_id", "/", "$$page"]},
+                    }
+                }
+            }
+        },
+    ]
+    return jsonify(mongo.db.galleries.aggregate(pipeline).next()["pages"])
+
+
+@root.route("/count-galleries", methods=["POST"])
 def count_galleries():
     pipeline = [{"$count": "count"}]
     return jsonify(mongo.db.galleries.aggregate(pipeline).next())
