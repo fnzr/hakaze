b7034291bb853f46d68592ef1f5cbf420af6abe3
fnzr@SANGVIS <5471818+fnzr@users.noreply.github.com>
2020-01-16 17:52:14 -0300

    Improve uvicorn deployment

--- hakaze/__init__.py
+++ hakaze/__init__.py
@@ -1,8 +1,9 @@
 from starlette.staticfiles import StaticFiles
 from fastapi import FastAPI
-
+from dotenv import load_dotenv
 
 def create_app():
+    load_dotenv()
     app = FastAPI()
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
