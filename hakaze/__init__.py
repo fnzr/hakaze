4c313dd33b34747cc7cc9786ae3ce8a2886ecaa2
fnzr@SANGVIS <5471818+fnzr@users.noreply.github.com>
2020-01-17 09:32:34 -0300

    Fixes for docker deployment

--- hakaze/__init__.py
+++ hakaze/__init__.py
@@ -2,11 +2,16 @@ from starlette.staticfiles import StaticFiles
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
+
 def create_app():
     load_dotenv()
     app = FastAPI()
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
     from . import routes
+
     app.include_router(routes.router)
     return app
+
+
+app = create_app()
